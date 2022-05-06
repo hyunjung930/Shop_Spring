d9d9870c8ec964858d6af34ca0bf82de172b27f3
hyunjungchoi <choihj930@gmail.com>
2022-05-06 17:51:30 +0900

    ItemRepositoryTest.java 생성 및 테스트 확인(save)

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -23,7 +23,7 @@ public class Item {
     private Long id; // 상품 코드
 
     @Column(nullable =false, length = 50)
-    private String itemN; //상품명
+    private String itemNm; //상품명
 
     @Column(name="price", nullable = false)
     private int price; //가격
