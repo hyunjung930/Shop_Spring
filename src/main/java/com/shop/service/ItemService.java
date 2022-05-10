bd9de6157d29e9d69569f14367edd39e7bfea362
hyunjungchoi <choihj930@gmail.com>
2022-05-10 22:30:28 +0900

    #6-2 상품 수정페이지 진입하기

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +25,7 @@ public class ItemService {
     private final ItemImgService itemImgService;
     private final ItemImgRepository itemImgRepository;
 
+    //상품 등록
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
         // 상품 등록
         Item item = itemFormDto.createItem();
@@ -37,5 +42,22 @@ public class ItemService {
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));    //상품의 이미지 정보 저장
         }
         return item.getId();
+    }
+
+    //등록된 상품 불러오기
+    @Transactional(readOnly = true) // 상품데이터를 읽는 트랜잭션을 읽기 전용으로 설정. jpa가 더디체킹(변경감지)을 수행하지 않아 성능 향상
+    public ItemFormDto getItemDtl(Long itemId){
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId); //해당 상품 이미지 조회. 상품 이미지 아이디를 오름 차순으로 가져옴
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for(ItemImg itemImg : itemImgList){ //조회한 ItemImg 엔티티를 ItemImgDto 객체로 만들어 리스트 추가
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        // 상품의 아이디를 통해 상품 엔티티를 조회, 존재하지 않을 때 EntityNotFoundException 발생
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
     }
 }
